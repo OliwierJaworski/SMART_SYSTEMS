b92b988785d1b0827e33efaa729576936916d8fa
OLiwier_Desktop <oliwier.jaworski@student.pxl.be>
2024-11-15 01:18:30 +0100

    added comments to __main__.py

--- Assignments/Assignment_3_/src/YOLO_11_MODEL/__main__.py
+++ Assignments/Assignment_3_/src/YOLO_11_MODEL/__main__.py
@@ -7,14 +7,21 @@ label_folder = "Data/obj_train_data"
 def main():
     print("Program execution started!\n")    
     print(os.getcwd())
-    # loading Pretrained model
-    #model = YOLO("yolo11n.pt")
-    model = YOLO(model="trained_models/trained_100_epochs.pt")
-    #results = model.train(data="Fles_dataset.yaml", epochs=100, imgsz=640)
-    #results = model.val()
-    #model.save("fles_model.pt")
+    # loading Pretrained model & train it 
+    model = YOLO("yolo11n.pt")
+    results = model.train(data="Fles_dataset.yaml", epochs=100, imgsz=640)
+    # or use your own trained one
+    # model = YOLO(model="trained_models/trained_100_epochs.pt")
+    # see the scores of your model after evaluation   
+    results = model.val()
+    # save model in the same directory as __main__.py
+    model.save("fles_model.pt")
+    # test model on new data
     results = model("Data/test/green_tea_pov.jpeg")
     result = results[0]
+    # store the data
     result.save("output/output_image5.jpg")
+    
+    
 if __name__ == "__main__":
     main()
