62a2119f4f33dd55a23c85d73b6099f79ecd3b43
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2024-12-03 14:55:15 +0100

    created dev branch -deleted dataset from directory

--- Assignments/Assignment_4_/model_training/src/__main__.py
+++ Assignments/Assignment_4_/model_training/src/__main__.py
@@ -1,28 +1,26 @@
 import os
 from ultralytics import YOLO
+import torch
 
 test_image_dir = "results/before"
-test_image_name = "cola.jpg"
+test_image_name = "frisdrank-fles1.jpg"
 result_image_dir = "results/after"
 
 pretrained = False
-pretrained_path = ""
-epochs = 20
-save_export = False
+pretrained_path = "trained.pt"
+epochs = 10
+save_export = True #inverted logic for some reason
 mode_save_name = "trained.pt"
 
 def main():
     if __debug__ : 
-        print("Program execution started!\n");   
+        print("Program execution started!\n")   
         print(os.getcwd())
-
     model = model_load()
-    results = train_model(model)
-    save_export(model=model)
-    model_test(model=model, results= results)
-      
+    training_results, validation_results = train_model(model)
+    save_export(model=model)  # Uncomment to enable export
+    model_test(model=model, results=validation_results)
 
-#model load in options
 def model_load():
     if not pretrained:
         model = YOLO("yolo11n.pt")
@@ -30,25 +28,24 @@ def model_load():
         model = YOLO(model=pretrained_path)
     return model
 
-#if train is enabled 
+# If training is enabled 
 def train_model(model):
-    if  not pretrained:
-        results = model.train(data="Fles_dataset.yaml", epochs=epochs, imgsz=640)
-        results = model.val()
-    return results
+    if not pretrained:
+        #training_results = model.train(data="Fles_dataset.yaml", epochs=epochs, imgsz=640)
+        training_results = model.train(data="Fles_dataset.yaml", epochs=epochs, imgsz=640)
+        validation_results = model.val()  # Separate validation results
+        return training_results, validation_results
 
-#if export is enabled
+# If export is enabled
 def save_export(model):
     if save_export:
         model.save(mode_save_name)
-        model.export(format="onnx",opset=11)
+        #model.export(format="onnx", opset=11)
 
-#will test the model on image provided 
+# Will test the model on image provided 
 def model_test(model, results):
-    model((test_image_dir+"/"+test_image_name))
-    result = results[0]
-    result.save((result_image_dir+"/"+test_image_name))
-
+    result = model(test_image_dir + "/" + test_image_name)
+    result[0].save(result_image_dir + "/" + test_image_name)
 
 if __name__ == "__main__":
     main()
